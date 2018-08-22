58ec1592f485741e9d7c45b9ca04c5268a168065
ZT-XU <1364904662@qq.com>
2018-08-22 06:57:15 +0000

    Add files via upload

---
+++ find_faces_in_picture_cnn.py
@@ -0,0 +1,54 @@
+from PIL import Image
+import face_recognition
+import picamera
+import numpy as np
+import openpyxl
+
+
+wb_name = ''
+res = input('是否创建一个新的签到表：(yes/no)')
+if 'yes' == res.lower():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    num = 0
+else:
+    wb_name = input('请输入已有的excel文件名：')
+    wb = openpyxl.load_workbook(wb_name + '.xlsx')
+    ws = wb.active
+    num = 1
+
+value_list = []
+
+ID = input("Enter your name:")
+for cells in ws['A']:
+    value_list.append(cells.value)
+    num = num + 1
+if ID not in value_list:
+    cell = ws.cell(row=num, column=1, value=ID)
+
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+while(ID):
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
+    face_locations = face_recognition.face_locations(output, number_of_times_to_upsample=0, model="cnn")
+
+
+    for face_location in face_locations:
+
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        
+
+        # You can access the actual face itself like this:
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        g_pil_image = pil_image.convert('L')
+        with open('./images' + ID + '.png', 'wb') as f:
+            f.write(g_pil_image)
+        print('Finsh!!!')
+        ID = False
+        
